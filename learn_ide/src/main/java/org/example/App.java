37d23afdc9b7387383dd96eab6744c582eac28b0
changschen <changschen@ebay.com>
2020-10-02 23:03:20 +0800

    Initial commit

---
+++ learn_ide/src/main/java/org/example/App.java
@@ -0,0 +1,24 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ * F7  项目源码
+ * Force step into 进入查看大牛源码
+ * 跳过，点击要直接跳到的代码初红点，按F9，
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        int a = 10;
+        int b = 20;
+        int c = 20;
+//        d = null;
+//        System.out.println(d.len());
+
+    }
+}
